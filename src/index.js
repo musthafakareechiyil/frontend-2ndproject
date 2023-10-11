92467674080637e7d51aedf1d3e9b29a702d3c8b
musthafa <musthafakareechiyil@gmail.com>
2023-10-11 15:33:31 +0530

    feedItem added to slice

--- src/index.js
+++ src/index.js
@@ -11,12 +11,10 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store = { store } >
-    <React.StrictMode>
       <BrowserRouter>
         <GoogleOAuthProvider clientId='439877519923-qpmbcqt3gb88svahhi65hnal1nkelfc6.apps.googleusercontent.com'>
           <App />
         </GoogleOAuthProvider>
       </BrowserRouter>
-    </React.StrictMode>
   </Provider>
 );
