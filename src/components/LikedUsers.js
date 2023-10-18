dd2ea9784a9656fc6a16dcfd57ffb6899a0886fc
musthafa <musthafakareechiyil@gmail.com>
2023-10-18 18:43:50 +0530

    liked list completed

--- src/components/LikedUsers.js
+++ src/components/LikedUsers.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { UserAxios } from '../config/Header_request';
 import { UserUrl } from '../APIs/BaseUrl';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function LikedUsers({ closeModal, feedItem }) {
   const userAxios = UserAxios();
   const [likedUsersData, setLikedUsersData] = useState(null);
+  const currentUser = useSelector((state) => state?.userDetails?.user?.id)
 
   useEffect(() => {
     const fetchLikedUsers = async () => {
@@ -25,16 +28,34 @@ function LikedUsers({ closeModal, feedItem }) {
     fetchLikedUsers();
   }, []);
 
+
   return (
-    <div
-      className='top-0 left-0 fixed h-full w-full backdrop-brightness-75 flex justify-center items-center'
-      onClick={closeModal}
-    >
-      <div className='absolute h-1/2 w-1/5 bg-gray-800 rounded-lg'>
+    <div className='top-0 left-0 fixed h-full w-full backdrop-brightness-75 flex justify-center items-center' onClick={closeModal}>
+      <div className='absolute max-h-96 w-1/6 bg-gray-800 rounded-lg p-4 overflow-y-auto'>
         {likedUsersData ? (
           <ul className='text-white'>
-            {likedUsersData?.map((user) => (
-              <li key={user.user.id}>{user.user.username}</li>
+            {likedUsersData.map((user) => (
+              <li key={user.user.id} className="mb-2">
+                <div className="flex items-center ">
+
+                  {/* profile image */}
+                  <img
+                    src={user.user.profile_url}
+                    alt={user.user.username}
+                    className="w-10 h-10 object-cover rounded-full m-2 mr-4"
+                  />
+
+                  {/* username */}
+                  <Link to={`/${user?.user?.username}`}
+                    className="text-white"
+                  >
+                    
+                    <p className='font-bold'>{user?.user?.username}</p>
+                    <p className='text-gray-400'>{user?.user?.fullname}</p>
+                  </Link>
+                  
+                </div>
+              </li>
             ))}
           </ul>
         ) : (
